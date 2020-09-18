e2a9f06057adfefa2ba3274932a1c21985e313b6
Tom Coemans <tom.coemans@gmail.com>
2020-09-18 17:43:59 +0200

    initial code

--- src/vl-info-tile.js
+++ src/vl-info-tile.js
@@ -1,4 +1,5 @@
 import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
+import '/node_modules/vl-ui-accordion/dist/vl-accordion.js';
 
 /**
  * VlInfoTile
@@ -12,6 +13,52 @@ import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-info-tile.html|Demo}
  *
  */
-export class VlInfoTile extends vlElement(HTMLElement) {}
+export class VlInfoTile extends vlElement(HTMLElement) {
+  constructor() {
+    super(`
+        <style>
+            @import '/src/style.css';
+        </style>
+        <div class="vl-info-tile">
+            <header class="vl-info-tile__header" role="presentation">
+                <div class="vl-info-tile__header__wrapper">
+                    <h3 class="vl-info-tile__header__title">
+                        <slot name="title"></slot>
+                    </h3>
+                    <p class="vl-info-tile__header__subtitle">
+                        <slot name="subtitle"></slot>
+                    </p>
+                </div>
+            </header>
+
+            <div class="vl-info-tile__content">
+                <slot name="content"></slot>
+            </div>
+
+            <footer class="vl-info-tile__footer">
+                <slot name="footer"></slot>
+            </footer>
+        </div>
+    `);
+  }
+
+  connectedCallback() {
+    this._processContent();
+  }
+
+  get _contentElement() {
+    return this._shadow.querySelector('.vl-info-tile__content');
+  }
+
+  get _contentSlot() {
+    return this.querySelector('[slot="content"]');
+  }
+
+  _processContent() {
+    if (!this._contentSlot) {
+      this._contentElement.remove();
+    }
+  }
+}
 
 define('vl-info-tile', VlInfoTile);
