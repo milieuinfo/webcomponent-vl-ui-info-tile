5d74e408bd023738e7af1327c8c8a8c298fb4962
Tom Coemans <tom.coemans@gmail.com>
2020-09-24 17:33:48 +0200

    added title label slot

--- src/vl-info-tile.js
+++ src/vl-info-tile.js
@@ -34,6 +34,7 @@ export class VlInfoTile extends vlElement(HTMLElement) {
                   <i class="vl-link__icon vl-link__icon--before vl-toggle__icon vl-vi vl-vi-arrow-right-fat" aria-hidden="true"></i>
                   <h3 id="title" class="vl-info-tile__header__title">
                     <slot name="title"></slot>
+                    <slot name="title-label></slot>
                   </h3>
                 </button>
                 <p class="vl-info-tile__header__subtitle">
