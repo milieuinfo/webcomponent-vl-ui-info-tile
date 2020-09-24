44d1f9c4960ca1e60ec5bc0e7187387dcda9b2ed
Tom Coemans <tom.coemans@gmail.com>
2020-09-24 17:11:58 +0200

    added toggleable functionality

--- src/vl-info-tile.js
+++ src/vl-info-tile.js
@@ -1,5 +1,5 @@
 import {vlElement, define} from '/node_modules/vl-ui-core/dist/vl-core.js';
-import '/node_modules/vl-ui-accordion/dist/vl-accordion.js';
+import '/lib/accordion.js';
 
 /**
  * VlInfoTile
@@ -14,31 +14,44 @@ import '/node_modules/vl-ui-accordion/dist/vl-accordion.js';
  *
  */
 export class VlInfoTile extends vlElement(HTMLElement) {
+  static get _observedAttributes() {
+    return ['toggleable'];
+  }
+
   constructor() {
     super(`
-        <style>
-            @import '/src/style.css';
-        </style>
-        <div class="vl-info-tile">
-            <header class="vl-info-tile__header" role="presentation">
-                <div class="vl-info-tile__header__wrapper">
-                    <h3 class="vl-info-tile__header__title">
-                        <slot name="title"></slot>
-                    </h3>
-                    <p class="vl-info-tile__header__subtitle">
-                        <slot name="subtitle"></slot>
-                    </p>
+      <style>
+        @import '/src/style.css';
+        @import '/node_modules/vl-ui-accordion/dist/style.css';
+      </style>
+      <div class="vl-info-tile">
+        <header class="vl-info-tile__header" role="presentation">
+          <div class="vl-info-tile__header__wrapper">
+            <div class="js js-vl-accordion">
+              <div class="vl-accordion" data-vl-accordion>
+                <button class="vl-toggle vl-link vl-link--bold" data-vl-accordion-toggle>
+                  <i class="vl-link__icon vl-link__icon--before vl-toggle__icon vl-vi vl-vi-arrow-right-fat" aria-hidden="true"></i>
+                  <h3 class="vl-info-tile__header__title">
+                    <slot name="title"></slot>
+                  </h3>
+                </button>
+                <p class="vl-info-tile__header__subtitle">
+                  <slot name="subtitle"></slot>
+                </p>
+                <div class="vl-accordion__content js-vl-accordion__content">
+                  <div class="vl-accordion__panel vl-info-tile__content">
+                    <slot name="content"></slot>
+                  </div>
                 </div>
-            </header>
-
-            <div class="vl-info-tile__content">
-                <slot name="content"></slot>
+              </div>
             </div>
+          </div>
+        </header>
 
-            <footer class="vl-info-tile__footer">
-                <slot name="footer"></slot>
-            </footer>
-        </div>
+        <footer class="vl-info-tile__footer">
+          <slot name="footer"></slot>
+        </footer>
+      </div>
     `);
   }
 
@@ -56,7 +69,13 @@ export class VlInfoTile extends vlElement(HTMLElement) {
 
   _processContent() {
     if (!this._contentSlot) {
-      this._contentElement.remove();
+      // this._contentElement.remove();
+    }
+  }
+
+  _toggleableChangedCallback(oldValue, newValue) {
+    if (newValue != undefined) {
+      vl.accordion.dress(this._element.querySelector('[data-vl-accordion]'));
     }
   }
 }
