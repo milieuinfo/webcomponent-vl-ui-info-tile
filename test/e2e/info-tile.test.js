e04e08dfe789392a3bb8c23599eabec713943841
Tom Coemans <tom.coemans@gmail.com>
2020-12-10 11:04:19 +0100

    casing

--- test/e2e/info-tile.test.js
+++ test/e2e/info-tile.test.js
@@ -9,25 +9,25 @@ describe('vl-info-tile', async () => {
     return vlInfoTilePage.load();
   });
 
-  it('Als gebruiker kan ik de titel van de info tile bekijken', async () => {
+  it('als gebruiker kan ik de titel van de info tile bekijken', async () => {
     const infoTile = await vlInfoTilePage.getInfoTile();
     await assert.eventually.equal(infoTile.getTitle(), 'Broos Deprez');
   });
 
-  it('Als gebruiker kan ik de subtitel van de info tile bekijken', async () => {
+  it('als gebruiker kan ik de subtitel van de info tile bekijken', async () => {
     const infoTile = await vlInfoTilePage.getInfoTile();
     await assert.eventually.include(infoTile.getSubtitle(), 'Uw zoon (19.05.2005)');
     await assert.eventually.include(infoTile.getSubtitle(), 'niet verwant aan referentiepersoon');
   });
 
-  it('Als gebruiker kan ik de content van de info tile bekijken', async () => {
+  it('als gebruiker kan ik de content van de info tile bekijken', async () => {
     const infoTile = await vlInfoTilePage.getInfoTile();
     const contentSlotElement = await infoTile.getContentSlotElement();
     await assert.eventually.include(contentSlotElement.getText(), 'Beslissingsbrief Studietoelage');
     await assert.eventually.include(contentSlotElement.getText(), 'De studietoelage voor Broos Deprez werd toegekend.');
   });
 
-  it('Als gebruiker kan ik een toggleable info tile openen en sluiten', async () => {
+  it('als gebruiker kan ik een toggleable info tile openen en sluiten', async () => {
     const infoTile = await vlInfoTilePage.getInfoTile();
     await assert.eventually.isFalse(infoTile.isOpen());
     await infoTile.toggle();
