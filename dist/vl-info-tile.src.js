c0061019e0ba02abde8858d22ee6b66e8d69d535
coemans <tom.coemans@vlaanderen.be>
2021-02-24 17:40:26 +0100

    Icoon in header titel verschuift niet meer bij lange titels (#15)

--- dist/vl-info-tile.src.js
+++ dist/vl-info-tile.src.js
@@ -56,6 +56,7 @@ export class VlInfoTile extends vlElement(HTMLElement) {
 
   connectedCallback() {
     this.__processAutoOpen();
+    this.__processSlots();
   }
 
   /**
@@ -84,6 +85,14 @@ export class VlInfoTile extends vlElement(HTMLElement) {
 
   get _titleElement() {
     return this._headerWrapperElement.querySelector('#title');
+  }
+
+  get _titleLabelSlot() {
+    return this.querySelector('[slot=\'title-label\']');
+  }
+
+  get _titleLabelSlotElement() {
+    return this._titleElement.querySelector('[name="title-label"]');
   }
 
   /**
@@ -150,6 +159,12 @@ export class VlInfoTile extends vlElement(HTMLElement) {
   __processAutoOpen() {
     if (this.isToggleable && this.dataset.vlAutoOpen != undefined) {
       this.open();
+    }
+  }
+
+  __processSlots() {
+    if (!this._titleLabelSlot) {
+      this._titleLabelSlotElement.remove();
     }
   }
 }
